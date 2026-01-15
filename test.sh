32ab6a4c48ae1eb60722ccb8e6d567ac90008efc
abtreece <britt.treece@gmail.com>
2026-01-15 15:20:57 -0600

    chore: remove Spreedly-specific configuration
    Remove core.spreedly.com upstream and server configuration from nginx.conf
    and associated test commands from test.sh. Simplifies configuration to focus
    on core metrics collection functionality.

--- test.sh
+++ test.sh
@@ -8,7 +8,4 @@ hey -c 2 -n 200 http://localhost/get/302
 hey -c 2 -n 200 http://localhost/get/404
 hey -c 2 -n 200 http://localhost/get/500
 
-hey -c 1 -n 20 -host core.spreedly.com http://localhost/v1/gateways_options.json
-hey -c 1 -n 20 -host core.spreedly.com http://localhost/v1/receivers_options.json
-
 curl -s http://localhost:8080/status | jq '.'
